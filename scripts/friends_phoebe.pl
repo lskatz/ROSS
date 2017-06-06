14d973450d7569ed6fb4997336566ea75ae9ad32
Lee Katz - Aspen <gzu2@cdc.gov>
2017-06-06 09:17:59 -0400

    max bases and max reads fixed

--- scripts/friends_phoebe.pl
+++ scripts/friends_phoebe.pl
@@ -3,6 +3,7 @@
 # TODO use ART for advanced things
 # TODO allow for basic ratios of ATCGN
 # TODO give better odds to certain read scores
+# TODO PE-interleaved awareness
 
 use strict;
 use warnings;
@@ -10,6 +11,7 @@ use File::Basename qw/basename/;
 use Getopt::Long;
 use POSIX qw/floor/;
 use List::Util qw/shuffle/;
+use Data::Dumper;
 
 use FindBin qw/$RealBin/;
 use lib "$RealBin/../lib/perl5";
@@ -21,21 +23,17 @@ use Thread::Queue;
 local $0=basename $0;
 my @nt=qw(A T C G);
 my $numNt=@nt;
-my @qual=();
-my $numQual=@qual;
 
 exit(main());
 
 sub main{
   my $settings={};
-  GetOptions($settings,qw(numcpus=i readlength=i numreads=i numbases=i tempdir=s help)) or die $!;
+  GetOptions($settings,qw(pe|paired-end numcpus=i readlength=i numreads=i numbases=i tempdir=s help)) or die $!;
   $$settings{numcpus}||=1;
   $$settings{readlength}||=150;
-  $$settings{numreads}||=1000;
   $$settings{tempdir}||=mktempdir();
   $$settings{numbases}||=0;
-
-  @qual=initialQualScores();
+  $$settings{numreads}||=0;
 
   die usage() if($$settings{help});
   
@@ -45,7 +43,7 @@ sub main{
   } 
   # If no reads are given, then just shuffle
   else {
-    $reads=generateRandomReads($$settings{numreads},$$settings{readlength},$settings);
+    $reads=generateRandomReads($$settings{readlength},$settings);
   }
   print $reads;
 
@@ -57,15 +55,34 @@ sub initialQualScores{
   for(0..40){
     push(@qual,chr($_+33));
   }
-  $numQual=@qual;
   return @qual;
 }
 
 sub generateRandomReads{
-  my($numreads,$readlength,$settings)=@_;
+  my($readlength,$settings)=@_;
+
+  # If number of bases is specified but not number of reads,
+  # set the number of reads to the max integer.
+  if($$settings{numbases} > 1 && $$settings{numreads}==0){
+    $$settings{numreads} = ~0; # max int
+  } 
+  # Same thing happens to the number of bases if the number
+  # of reads have been set
+  elsif( $$settings{numreads} > 1 && $$settings{numbases}==0){
+    $$settings{numbases} = ~0;
+  } 
+  # If neither are set, then just set them both to 1.
+  elsif( $$settings{numreads}==0 && $$settings{numbases}==0){
+    $$settings{numreads}=1;
+    $$settings{numbases}=1;
+  }
+
+  my @qual=initialQualScores();
+  my $numQual=@qual;
 
   my $readStr="";
-  for(my $i=0;$i<$numreads;$i++){
+  my $basesCount=0;
+  for(my $i=0;$i<$$settings{numreads};$i++){
     my $seq ="";
     my $qual="";
     for(my $j=0;$j<$readlength;$j++){
@@ -74,6 +91,9 @@ sub generateRandomReads{
     }
 
     $readStr.='@read'.$i."\n".$seq."\n"."+\n".$qual."\n";
+
+    $basesCount+=length($seq);
+    last if($basesCount > $$settings{numbases});
   }
 
   return $readStr;
@@ -91,6 +111,17 @@ sub randomizeReads{
       my $plus=<$fh>;
       my $qual=<$fh>;
       my $read=$id.$sequence.$plus.$qual;
+
+      if($$settings{pe}){
+        my $id2=<$fh>;
+        my $sequence2=<$fh>;
+        my $plus2=<$fh>;
+        my $qual2=<$fh>;
+        $read.=$id2.$sequence2.$plus2.$qual2;
+        # keep track of the number of bases
+        chomp($sequence2);
+        $sequence.=$sequence2;
+      }
       push(@readEntry,$read);
 
       # Record the number of bases per read
@@ -121,13 +152,21 @@ sub usage{
   "$0: Generate random reads or shuffle reads
   Usage: 
          # Completely random reads
-         $0 --numreads 100 --readlength 150
+         $0 --numreads 100 --numbases 9999 --readlength 150
+
+         # A single random read
+         $0 
 
          # Get reads from a real fastq file; do not truncate.
          $0 --numreads 100 file.fastq[.gz]
 
          # 1x coverage for many bacterial genomes
          $0 --numbases 5000000 file.fastq[.gz]
+
+  OPTIONS
+  --pe              paired end
+  --numreads  0     Maximum number of reads.
+  --numbases  0     Maximum number of nucleotides.
   "
 }
 
