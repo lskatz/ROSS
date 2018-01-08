41f4c3ac0196625c1dfc956544ef155e89e0f641
Lee Katz <lskatz@gmail.com>
2018-01-08 12:43:38 -0500

    not quite done with the iterator

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,26 @@
 extern crate ross;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 
 fn main(){
     
     ross::parse_args();
 
-    ross::read_fastq_carefully();
+    //let stdin = std::io::stdin();
+    let stdin = File::open("/dev/stdin").expect("Could not open /dev/stdin");
+    let reader = BufReader::new(&stdin);
+    let fastq_iter = ross::ReadFastqCarefully::new(&reader);
 
+    for seq in fastq_iter {
+      //println!("{}\n{}\n{}",seq.id.trim(),seq.seq,seq.qual.trim());
+      println!("{}\n",seq.id.trim());
+    }
+
+    /*
+    let reads :Vec<ross::Seq> = ross::read_fastq_carefully();
+
+    println!(">{}<",reads[0].id);
+    */
 }
 
