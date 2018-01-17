0e4c1066545c6b5c12ca3a14c077d2345ad10334
Lee Katz <lskatz@gmail.com>
2018-01-16 19:37:18 -0500

    rs files for binaries

---
+++ rust/src/bin/friends_rachel.rs
@@ -0,0 +1,26 @@
+extern crate ross;
+extern crate statistical;
+
+use std::fs::File;
+use std::io::BufReader;
+
+use ross::io::fastq;
+
+use statistical::mean;
+
+fn main(){
+    
+    //ross::parse_args();
+
+    let my_file = File::open("/dev/stdin").expect("Could not open file");
+    let my_buffer=BufReader::new(my_file);
+    let mut fastq_reader=fastq::Reader::new(my_buffer);
+    let mut read_length = Vec::new();
+    while let Some(seq_obj) = fastq_reader.read_carefully() {
+      let len = seq_obj.seq.len() as f32;
+      read_length.push(len);
+    }
+    
+    println!("{}", mean(&read_length));
+}
+
