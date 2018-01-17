cca826c4b02b4bf943453f914180f0f83560b6c7
Lee Katz <lskatz@gmail.com>
2018-01-16 20:50:47 -0500

    avgqual

--- rust/src/bin/friends_rachel.rs
+++ rust/src/bin/friends_rachel.rs
@@ -17,10 +17,19 @@ fn main(){
     let mut fastq_reader=fastq::Reader::new(my_buffer);
     let mut read_length = Vec::new();
     while let Some(seq_obj) = fastq_reader.read_carefully() {
+      // length of read
       let len = seq_obj.seq.len() as f32;
       read_length.push(len);
+
+      // quality
+      let qual :Vec<f32> = seq_obj.qual.chars()
+                                .map(|x| {x as u8 as f32-33.0})
+                                .collect();
+      let avgQual = mean(&qual);
+      println!("{:?}",avgQual);
+      break;
     }
     
-    println!("{}", mean(&read_length));
+    println!("Mean read length {}", mean(&read_length));
 }
 
