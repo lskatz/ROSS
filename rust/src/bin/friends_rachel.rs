74165fd80e0a22f1dd2d998e9994e0b6cf9d3646
Lee Katz <lskatz@gmail.com>
2018-01-16 22:13:59 -0500

    rachel simple version

--- rust/src/bin/friends_rachel.rs
+++ rust/src/bin/friends_rachel.rs
@@ -16,20 +16,45 @@ fn main(){
     let my_buffer=BufReader::new(my_file);
     let mut fastq_reader=fastq::Reader::new(my_buffer);
     let mut read_length = Vec::new();
+    let mut avg_qual = Vec::new();
+    let mut num_bases :f32 = 0.0;
+    let mut min_read_length :f32 = std::f32::MAX;
+    let mut max_read_length :f32 = 0.0;
+    let mut num_reads :usize = 0;
     while let Some(seq_obj) = fastq_reader.read_carefully() {
+      num_reads+=1;
+
       // length of read
       let len = seq_obj.seq.len() as f32;
       read_length.push(len);
+      num_bases += len;
+
+      if len > max_read_length {
+        max_read_length = len;
+      }
+      if len < min_read_length {
+        min_read_length = len;
+      }
 
       // quality
       let qual :Vec<f32> = seq_obj.qual.chars()
                                 .map(|x| {x as u8 as f32-33.0})
                                 .collect();
-      let avgQual = mean(&qual);
-      println!("{:?}",avgQual);
-      break;
+      avg_qual.push(mean(&qual));
     }
     
-    println!("Mean read length {}", mean(&read_length));
+    println!("{}",vec![
+        "read_length", "num_bases",
+        "min_read_length", "max_read_length",
+        "avg_quality", "num_reads", "PE?",
+        "coverage",
+    ].join("\t"));
+    println!("{}",vec![
+        mean(&read_length).to_string(),
+        num_bases.to_string(),
+        min_read_length.to_string(), max_read_length.to_string(),
+        mean(&avg_qual).to_string(), num_reads.to_string(), 
+        "0".to_string(),
+    ].join("\t"));
 }
 
